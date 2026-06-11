68099020f5a960e806901f328863d815e1582420
rafaellacamarrichang <rafaellagss8@gmail.com>
2026-06-10 23:50:32 -0300

    Homepage version inicial y componente Navbar

---
+++ src/components/layouts/NavBarComponent.jsx
@@ -0,0 +1,74 @@
+import "./NavbarComponent.css";
+import { Link } from "react-router-dom";
+function NavbarComponent() {
+  return (
+    <nav className="navbar navbar-expand mi-navbar">
+      <div className="container-fluid">
+
+        <Link className="navbar-brand" to="/">
+          AccesibleMENTE
+        </Link>
+
+        <ul className="navbar-nav ms-auto flex-row">
+
+          <li className="nav-item mx-3">
+            <Link className="nav-link" to="/">
+              Sobre Nosotros
+            </Link>
+          </li>
+
+          <li className="nav-item dropdown mx-3">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Profesionales
+            </a>
+
+            <ul className="dropdown-menu">
+              <li>
+                <Link
+                  className="dropdown-item"
+                  to="/profesionales/psicologia"
+                >
+                  Psicología
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className="dropdown-item"
+                  to="/profesionales/psiquiatria"
+                >
+                  Psiquiatría
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className="dropdown-item"
+                  to="/profesionales/psicopedagogia"
+                >
+                  Psicopedagogía
+                </Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav-item mx-3">
+            <Link className="nav-link" to="/">
+              Iniciar/Registrarse
+            </Link>
+          </li>
+
+        </ul>
+
+      </div>
+    </nav>
+  );
+}
+
+export default NavbarComponent;
