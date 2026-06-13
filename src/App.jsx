33086ea5ee5d0c3172c371a9e77fb35cf6d44d3a
Alain Cluzet <acluzet19@gmail.com>
2026-06-12 21:41:21 -0300

    arreglo de cosillas

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavbarComponent from "./components/layouts/NavBarComponent";
 import AboutUs from "./pages/AboutUs";
+import Professionals from "./pages/Professionals";
 function App() {
   return (
     <BrowserRouter>
       <NavbarComponent />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sobre-nosotros" element={<AboutUs />}></Route>
+        <Route path="/sobre-nosotros" element={<AboutUs />} />
+        <Route path="/profesionales" element={<Professionals />} />
       </Routes>
     </BrowserRouter>
   );
