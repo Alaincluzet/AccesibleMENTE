26330ea5fe993690d5ba4aef92ee17667d6700b2
Alain Cluzet <acluzet19@gmail.com>
2026-06-15 20:54:55 -0300

    realicé segunda parte de la sección y cambié colores

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,3 +1,4 @@
+import { Link, Links } from "react-router-dom";
 import NavbarComponent from "../components/layouts/NavBarComponent";
 import "./AboutUs.css";
 function AboutUs() {
@@ -8,17 +9,21 @@ function AboutUs() {
           <h1>¿Cómo funciona AccesibleMENTE?</h1>
           <p>
             Accesiblemente es una red pensada para agilizar el proceso de
-            encontrar un terapeutas a precios accesibles, el funcionamiento es
-            sencillo: -Accedes al portal donde se encuentran todos nuestros
-            profesionales capacitados en diferentes áreas de la psicología.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            omnis laboriosam impedit dignissimos esse expedita consequatur
-            excepturi, mollitia eaque dolores id quidem exercitationem tempora
-            dolorem aliquid. Eveniet quidem commodi magnam?
+            encontrar un terapeuta a precios accesibles, el funcionamiento es
+            sencillo:
+          </p>
+          <ul>
+            <li>-Creas tu usuario</li>
+            <li>-Buscás el terapeuta que se adecúe a tus necesidades.</li>
+            <li>-Listo, empezas tu proceso terapéutico.</li>
+          </ul>
+          <p id="text">
+            ¿No contas con recursos? Ponete en contacto con nosotros y evaluamos
+            tu caso.
           </p>
         </div>
         <div className="about-imagen">
-          <span>imagen</span>
+          <img src="/hero.png" alt="sesion-de-terapia" />
         </div>
       </section>
       <section className="about-cards">
@@ -50,6 +55,35 @@ function AboutUs() {
             Fernandez y Alain Cluzet
           </p>
         </article>
+      </section>
+      <section className="about-info">
+        <div className="about-info-box">
+          <h1>
+            {" "}
+            ¿Qué es <u>AccesibleMENTE?</u>
+          </h1>
+          <p>
+            AccesibleMENTE nace de una idea colectiva de 3 estudiantes de
+            desarrollo web que participan en conjunto para suplir una necesidad
+            en la población. Realizamos encuestas y los datos que manifestaron
+            fueron casi unánimes, mucha gente quiere mejorar su salud mental y
+            el mayor problema siempre era el económico, aparte de otras razones
+            como falta de tiempo, largas esperas para recibir turno. Por eso
+            creamos accesibleMENTE una red que conecta profesionales de la salud
+            mental con la gente. Muchos se cuestionaran ¿Qué la hace tan
+            especial? Nosotros no solo pensamos en el dinero, la membresía que
+            los profesionales van a adquirir viene con una pequeña cláusula que
+            está clarísima desde el primer momento, a cada profesional vamos a
+            pedirles que nos donen 4 horas a la semana pro-bono para aquellos
+            que no pueden acceder por problemas económicos.
+          </p>
+        </div>
+      </section>
+      <section className="about-register">
+        <p>
+          Unite a esta causa solidaria -{" "}
+          <Link to="/registro">Registrate aquí</Link>
+        </p>
       </section>
     </main>
   );
